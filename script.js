a113f42a7a683ab431ab4a67232f350c1de7e3cf
Nandakumar ms <nandakumarms10@gmail.com>
2023-08-09 16:06:05 +0530

    new branch

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+var flag=false
+
 function buttonclick(val)
 {
-    document.getElementById("screen").value+=val;
+    var screen= document.getElementById("screen");
+    if(val == '.' && flag != true ){
+        screen.value+=val;
+        flag=true;
+    }else if(val == '+' || val == '-' || val == '*' || val == '/'){
+    
+        screen.value+=val 
+        flag=false;
+    }else{
+
+        screen.value+=val
+    }
+
 }
 function equalclick()
+
 {
+
+    flag=false;
     var text=document.getElementById("screen").value
 
     var result=eval(text)
